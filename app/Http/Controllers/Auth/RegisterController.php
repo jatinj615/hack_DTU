63d79329e0d6ad07e3adfe66ea6076e68356a2ab
jatinj615 <jatinj615@gmail.com>
2017-10-14 04:30:36 +0530

    models created

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
             'phone' => $data['phone']
+
         ]);
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('phone');
+            $table->string('phone')->unique();
             $table->integer('amount')->default(0);
             $table->boolean('active')->default(false);
             $table->rememberToken();
